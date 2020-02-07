817fca62649bca166daf34292e491c00f30d39b8
Habeeb Okunade <guessy2k2@yahoo.com>
2020-02-07 19:56:14 +1100

    Building Project

---
+++ src/test/java/com/habeebcycle/jenkinsdocker/JenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habeebcycle.jenkinsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
